b19287f5cb070a9203848bd0ff40573fb8931ca0
Ben Bal <bensimonbal@gmail.com>
2022-04-25 14:16:32 +0200

    Add project files.

---
+++ EFCoreRelationships/Controllers/CharacterController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EFCoreRelationships.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CharacterController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public CharacterController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Character>>> Get(int userId)
+        {
+            var characters = await _context.Characters
+                .Where(x => x.UserId == userId)
+                .Include(x => x.Weapon)
+                .Include(x => x.Skills)
+                .ToListAsync();
+
+            return characters;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Character>>> Create(CreateCharacterDto request)
+        {
+            var user = await _context.Users.FindAsync(request.UserId);
+            if (user == null)
+                return NotFound();
+
+            var newCharacter = new Character
+            {
+                Name = request.Name,
+                RpgClass = request.RpgClass,
+                User = user
+            };
+
+            _context.Characters.Add(newCharacter);
+            await _context.SaveChangesAsync();
+
+            return await Get(newCharacter.UserId);
+        }
+
+        [HttpPost("weapon")]
+        public async Task<ActionResult<Character>> AddWeapon(AddWeaponDto request)
+        {
+            var character = await _context.Characters.FindAsync(request.CharacterId);
+            if (character == null)
+                return NotFound();
+
+            var newWeapon = new Weapon
+            {
+                Name = request.Name,
+                Damage = request.Damage,
+                Character = character
+            };
+
+            _context.Weapons.Add(newWeapon);
+            await _context.SaveChangesAsync();
+
+            return character;
+        }
+
+        [HttpPost("skill")]
+        public async Task<ActionResult<Character>> AddSkill(SkillDto request)
+        {
+            var character = await _context.Characters
+                .Where(x => x.Id == request.CharacterId)
+                .Include(x => x.Skills)
+                .FirstOrDefaultAsync();
+
+            if (character == null)
+                return NotFound();
+
+            var skill = await _context.Skills.FindAsync(request.SkillId);
+            if (skill == null)
+                return NotFound();
+
+            character.Skills.Add(skill);
+            await _context.SaveChangesAsync();
+
+            return character;
+        }
+    }
+}
